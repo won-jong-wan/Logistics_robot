33fc778fe89cb276bc049ba47e708d4577f1c472
YUJJHH <money7339@naver.com>
2024-05-12 01:30:56 +0900

    알고리즘 경로

---
+++ 0511_test/Core/Inc/pid.h
@@ -0,0 +1,22 @@
+#include <stdint.h>
+
+
+
+void position_pid_x(void);
+void position_pid_y(void);
+
+void v_t_graph(float targetS);
+
+void dc_motor_pid_X(float input_encoder);
+void dc_motor_pid_Y(float input_encoder) ;
+
+void dc_motor_RPM(void);
+
+void v_t_graph_DIR_GO_X(float targetS);
+void v_t_graph_DIR_BACK_X(float targetS);
+
+void v_t_graph_DIR_GO_Y(float targetS_Y);
+void v_t_graph_DIR_BACK_Y(float targetS_Y);
+
+void VT_control_X(void);
+void VT_control_Y(void);
---
+++ 0511_test/Core/Inc/sensor.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+
+
+void distance_sensor (void);
+
+void microDelay(uint16_t delay);
